120b3178538086d9563fc6926edcd451d3523dcd
Rev-You <cezartigley@gmail.com>
2024-01-08 19:28:27 +0800

    updates

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 const MainContent = () => {
+  const hasToken = sessionStorage.getItem('token');
+
+  const link = hasToken ? '/reviewer-maker' : '/login';
+
   return (
     <>
-        <div className="main-content container">
-      <div>
-      <h1>Study easier, study smarter.</h1>
-      <h3>RevYou is a website that automatically <br>
-      </br> creates reviewers for students who wish <br></br>to make their studying process easier.</h3>
-      <br></br>
-      <a href= '/reviewer-maker'className="green-btn">Try it now!</a>
+      <div className="main-content container">
+        <div>
+          <h1>Study easier, study smarter.</h1>
+          <h3>
+            RevYou is a website that automatically
+            <br />
+            creates reviewers for students who wish
+            <br />
+            to make their studying process easier.
+          </h3>
+          <br />
+          <Link to={link} className="green-btn">
+            Try it now!
+          </Link>
+        </div>
       </div>
-
-    </div>
-    <div className='main-content-footer'>
-      <Footer />
+      <div className='main-content-footer'>
+        <Footer />
       </div>
     </>
-
   );
 };
 
